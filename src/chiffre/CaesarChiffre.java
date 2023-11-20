465ad88fa887cdea87ef3af9f2644e7c430eb8a9
JT-808 <jerome.tengg@gmx.de>
2023-11-20 21:59:50 +0100

    sakjdhk

--- src/chiffre/CaesarChiffre.java
+++ src/chiffre/CaesarChiffre.java
@@ -2,19 +2,24 @@ package chiffre;
 
 public class CaesarChiffre {
 
-    private String zeichen;
+    private String zeichen = "ABCEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public String verschlüsseln(String klartext, int verschieben) {
+    public String verschluesseln(String klartext, int verschiebung) {
         String verschlüsselterText = "";
         for (int i = 0; i < klartext.length(); i++) {
             char next = klartext.charAt(i);
-            verschlüsselterText = verschlüsselterText.verschieben(next, verschiebung);
+            verschlüsselterText = verschlüsselterText + verschieben(next, verschiebung);
         }
         return verschlüsselterText;
     };
 
     private char verschieben(char buchstabe, int verschiebung) {
-
+        int posBuchstabe = zeichen.indexOf(buchstabe);
+        posBuchstabe += verschiebung;
+        while (posBuchstabe > zeichen.length() - 1) {
+            posBuchstabe -= zeichen.length();
+        }
+        return zeichen.charAt(posBuchstabe);
     };
 
 }
--- src/chiffre/Tester.java
+++ src/chiffre/Tester.java
@@ -1,9 +1,23 @@
 package chiffre;
 
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
-        String Klartext = "Hallo";
-        System.out.println(Klartext);
+        // String Klartext = "Hallo";
+        int verschiebung = 2;
+
+        Scanner wort = new Scanner(System.in);
+
+        System.out.println("Bitte Wort eingeben");
+        String Klar;
+        Klar = wort.nextLine();
+
+        System.out.println("unverschlüsselt: " + Klar);
+        System.out.println("Länge: " + Klar.length());
+        System.out.println("der letzten beiden Buchstaben waren: " + Klar.substring(Klar.length() - 2));
+
+        String verschluesselterText = Klar.verschluesseln(Klar, verschiebung);
 
     }
 }
