3c6e6fab0f964df09562a128c7d7d682d0d65521
JT-808 <jerome.tengg@gmx.de>
2024-01-12 10:21:36 +0100

    changing muenzwurf

--- src/Probeklausur1/muenzwurf.java
+++ src/Probeklausur1/muenzwurf.java
@@ -6,28 +6,19 @@ public class muenzwurf {
 
     public static void main(String[] args) {
 
-        wurf(2);
+        wurf(5);
     }
-
-    int Wuerfe;
 
     public static void wurf(int Wuerfe) {
 
         String[] Wurfergebniss = new String[Wuerfe];
-
-        Random random = new Random();
         for (int i = 0; i < Wuerfe; i++) {
-            int ZZahl = random.nextInt(2); // 0-1
-
-            String Ergebnis = (ZZahl == 0) ? "wappen" : "Zahl";
-
+            int ZZahl = new Random().nextInt(2); // 0-1
+            String Ergebnis = (ZZahl == 0) ? "Wappen" : "Zahl";
             Wurfergebniss[i] = "Wurf " + (i + 1) + " = " + Ergebnis;
-
         }
         for (String string : Wurfergebniss) {
             System.out.println(string);
-
         }
-
     }
 }
