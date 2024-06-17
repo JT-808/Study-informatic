127f8eef1cf2edf754b38ac03ce9405e316f9b42
JT-808 <jerome.tengg@gmx.de>
2024-06-17 21:30:18 +0200

    finish Suchbaum

--- src/second_semester/BauemeUndGraphen/Uebungen/Suchbaum/SuchBaum.java
+++ src/second_semester/BauemeUndGraphen/Uebungen/Suchbaum/SuchBaum.java
@@ -116,39 +116,92 @@ public class SuchBaum {
 
 
 
-
+	public void delete(int key) {
+	    if(member(key)) {
+		Node loeschen=root;
+		Node ElternKnoten=null;
+		
+		while(loeschen != null && loeschen.getKey()!=key) {//ElternKnoten herausfinden
+		    ElternKnoten=loeschen;
+		    if(key<loeschen.getKey()) {
+			loeschen=loeschen.getLchild();
+		    }else if(key>loeschen.getKey()) {
+			loeschen=loeschen.getRchild();
+		    }  
+		    //-->Elternknoten von zu löschendem Knoten ==ElternKnoten
+		}
+		
+		if (loeschen!=find(key)) {//nur mal Prüfen
+		    System.out.println(loeschen+"      "+find(key));
+		    throw new IndexOutOfBoundsException("Fehler");
+		}
+		
+		if (loeschen.getLchild()==null||loeschen.getRchild()==null) {//wenn (nur) rechter oder linker KindKnoten exsistiert
+		    Node einzelKnoten=null;
+		    if (loeschen.getLchild()==null) {//wenn nur der Rechte existiert
+			einzelKnoten=loeschen.getRchild();
+		    }else{//wenn nur der Rechte existiert
+			einzelKnoten=loeschen.getLchild();
+		    }
+		    if(loeschen==root) {
+			root=einzelKnoten;
+		    }else if (key < ElternKnoten.getKey()) {//wenn im linken Teilbaum
+			ElternKnoten.setLchild(einzelKnoten);
+		    }else {//wenn im rechten Teilbaum
+			ElternKnoten.setRchild(einzelKnoten);
+		    }
+		//wenn beide Bäume etwas enthalten
+		}else if ((loeschen.getLchild()!=null)&&(loeschen.getRchild()!=null)) {
+		    Node kleinstRKey=loeschen.getRchild();
+		    Node node=loeschen;
+		    while(kleinstRKey.getLchild()!=null) {//im rechten Teilbaum Kleinsten Wert suchen
+			node=kleinstRKey;
+			kleinstRKey=kleinstRKey.getLchild();
+		    }
+		    loeschen.setKey(kleinstRKey.getKey());
+		    if (kleinstRKey == node.getRchild()) {//den zu löschenden Knoten ersetzen mit dem Kleinsten im Teilbaum			    
+			loeschen.setRchild(kleinstRKey.getRchild());
+		    }
+		    else {//für den linken TB
+			node.setLchild(kleinstRKey.getRchild());
+		    }
+		}		
+	    }else {
+		throw new IndexOutOfBoundsException("Der knoten ist nicht im Baum :(");
+	    }  
+	}
 
 /*
  * 
  * 
- *  NOCH BEENDEND!
+ *  NOCH BEENDEND! -> Mein Versuch:
  * 
  */
-	public void delete(int key) {
-		//wenn gesuchte Zahl im Baum gefunden (=true)
-		if (member(key)) {
-			if(root.getLchild()== null && root.getRchild()==null){
-				root=null;
+	// public void delete(int key) {
+	// 	//wenn gesuchte Zahl im Baum gefunden (=true)
+	// 	if (member(key)) {
+	// 		if(root.getLchild()== null && root.getRchild()==null){
+	// 			root=null;
 				
-			}else if (root.getLchild()==null){
-				root=root.getRchild();
+	// 		}else if (root.getLchild()==null){
+	// 			root=root.getRchild();
 				
-			}else if (root.getRchild()==null){
-				root=root.getLchild();
-			}else{
+	// 		}else if (root.getRchild()==null){
+	// 			root=root.getLchild();
+	// 		}else{
 
-				if(key < root.getKey()) {
-					SuchBaum left = new SuchBaum(root.getLchild());
-					left.delete(key);
-					root.setLchild(left.root);
-				}
-				// Fall 2: rechter Teilbaum ist leer
-				else if (key > root.getKey()) {
-					SuchBaum right = new SuchBaum(root.getRchild());
-					right.delete(key);
-					root.setRchild(right.root);
-				}
-			}
+	// 			if(key < root.getKey()) {
+	// 				SuchBaum left = new SuchBaum(root.getLchild());
+	// 				left.delete(key);
+	// 				root.setLchild(left.root);
+	// 			}
+	// 			// Fall 2: rechter Teilbaum ist leer
+	// 			else if (key > root.getKey()) {
+	// 				SuchBaum right = new SuchBaum(root.getRchild());
+	// 				right.delete(key);
+	// 				root.setRchild(right.root);
+	// 			}
+	// 		}
 
 			// if (root.getLchild() == null) {
             //     root.setLchild(root.getRchild());
@@ -162,8 +215,8 @@ public class SuchBaum {
 				
 			// }		
 		
-		} else {
-			System.out.println("nicht vorhanden");
-		}
-	}
+		// } else {
+		// 	System.out.println("nicht vorhanden");
+		// }
+	//}
 }
