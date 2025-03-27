0fec9b93749a4d6ee672b1342fba95c5347c1d5a
JT-808 <jerome.tengg@gmx.de>
2025-03-27 09:20:33 +0100

    release aauf Linux

---
+++ Csharp/dotNetScripte.cs
@@ -0,0 +1,24 @@
+Console.WriteLine("Test");
+
+
+/*
+Falls du dein Skript ohne Kompilierung direkt ausführen möchtest:
+
+Installiere das dotnet-script-Paket:
+****
+dotnet tool install -g dotnet-script
+****
+______
+
+Führe das Skript direkt aus:
+*****
+dotnet script dotNetScripte.cs
+*****
+______
+
+Vorteile von dotnet script:
+Du brauchst keine .csproj-Datei.
+Es funktioniert ohne explizites Kompilieren.
+Ideal für kleine Skripte und Experimente.
+*/
+
---
+++ Csharp/releaseAufLinux.cs
@@ -0,0 +1,57 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, .NET 9!");
+    }
+}
+
+
+
+/*
+
+für release auf linux:
+
+-> Main Methode wird benötigt!
+-> benötigt eine .csproj datei mit folgendem Inhalt:
+
+<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <TargetFramework>net9.0</TargetFramework>
+    <ImplicitUsings>enable</ImplicitUsings>
+    <Nullable>enable</Nullable>
+  </PropertyGroup>
+</Project>
+
+
+Kompilieren & ausführen
+––––––––––––––––––––––––
+Jetzt kannst du das Programm ausführen oder eine ausführbare Datei erstellen.
+
+A) Direkt ausführen
+****
+dotnet run
+****
+B) Native Linux-Binary erzeugen (ohne .NET-Abhängigkeit)
+****
+dotnet publish -c Release -r linux-x64 --self-contained -o out
+****
+Dann kannst du die Datei starten:
+./out/hello
+
+*****
+dotnet publish -c Release -r linux-x64 --self-contained
+*****
+
+-c Release: Verwendet die Release-Konfiguration für die Veröffentlichung.
+-r linux-x64: Gibt die Zielplattform an (in diesem Fall Linux x64).
+--self-contained: Stellt sicher, dass die benötigten .NET-Abhängigkeiten in die Veröffentlichung eingebunden werden, sodass der Code ohne eine installierte .NET-Laufzeit auf anderen Systemen ausgeführt werden kann.
+
+
+chmod +x HelloApp 
+./HelloApp
+
+*/
