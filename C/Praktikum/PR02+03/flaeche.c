97ccfd823dac26e9618461f8f3c81ff00db58b87
JT-808 <jerome.tengg@gmx.de>
2025-04-09 15:13:12 +0200

    finish pr02

---
+++ C/Praktikum/PR02+03/flaeche.c
@@ -0,0 +1,10 @@
+#define PI 3.1415926
+
+int anzahl=0;
+
+/* Definition der Funktion kreisFlaeche() */
+double kreisFlaeche(double radius)
+{
+    anzahl++;
+    return PI*radius*radius;
+}
---
+++ C/Praktikum/PR02+03/flaeche.h
@@ -0,0 +1,5 @@
+#define PI 3.1415926
+
+extern int anzahl;
+
+double kreisFlaeche(double radius);
